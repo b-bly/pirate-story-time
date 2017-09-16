5ba49d05bfb8931965cf1564817ff9f4ee2f09fe
Brendt Bly <brendt.bly@outlook.com>
2017-09-16 07:07:51 -0500

    changed password authentication and routing after login

--- server/public/scripts/controllers/login.controller.js
+++ server/public/scripts/controllers/login.controller.js
@@ -15,17 +15,13 @@ myApp.controller('LoginController', function($http, $location, UserService) {
       console.log('LoginController -- login');
       if(self.user.username === '' || self.user.password === '') {
         self.message = "Enter your username and password, ye scurvy dog!";
-      } else if (self.user.password.length < 6) {
-        self.message = "Yer password must be at least 6 characters long, matey."
-      // } else if (self.user.password != self.user.passwordConfirm) {
-      //   self.message = "Arr, the passwords don't match!"
-      } else {
+      }  else {
         console.log('LoginController -- login -- sending to server...', self.user);
         $http.post('/', self.user).then(function(response) {
           if(response.data.username) {
             console.log('LoginController -- login -- success: ', response.data);
             // location works with SPA (ng-route)
-            $location.path('/user'); // http://localhost:5000/#/user
+            $location.path('/story'); // http://localhost:5000/#/user
           } else {
             console.log('LoginController -- login -- failure: ', response);
             self.message = "Wrong!!";
@@ -42,6 +38,10 @@ myApp.controller('LoginController', function($http, $location, UserService) {
       console.log('LoginController -- registerUser');
       if(self.user.username === '' || self.user.password === '') {
         self.message = "Choose a username and password!";
+      } else if (self.user.password.length < 6) {
+        self.message = "Yer password must be at least 6 characters long, matey."
+      // } else if (self.user.password != self.user.passwordConfirm) {
+      //   self.message = "Arr, the passwords don't match!"
       } else {
         console.log('LoginController -- registerUser -- sending to server...', self.user);
         $http.post('/register', self.user).then(function(response) {
--- server/public/scripts/services/user.service.js
+++ server/public/scripts/services/user.service.js
@@ -7,12 +7,12 @@ myApp.factory('UserService', function($http, $location){
     userObject : userObject,
 
     getuser : function(){
-      console.log('UserService -- getuser');
+      //console.log('UserService -- getuser');
       $http.get('/user').then(function(response) {
           if(response.data.username) {
               // user has a curret session on the server
               userObject.userName = response.data.username;
-              console.log('UserService -- getuser -- User Data: ', userObject.userName);
+              //console.log('UserService -- getuser -- User Data: ', userObject.userName);
           } else {
               console.log('UserService -- getuser -- failure');
               // user has no session, bounce them back to the login page
