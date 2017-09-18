f0d92c92a003ef03706ca07551b3fe4c4215adb7
Brendt Bly <brendt.bly@outlook.com>
2017-09-18 11:57:41 -0500

    styling

--- server/public/scripts/services/card.service.js
+++ server/public/scripts/services/card.service.js
@@ -106,5 +106,20 @@ myApp.service('CardService', ['$http', '$location', function ($http, $location) 
         });
     }
 
+    // swal({
+    //     title: 'Are you sure?',
+    //     text: "You won't be able to revert this!",
+    //     type: 'warning',
+    //     showCancelButton: true,
+    //     confirmButtonColor: '#3085d6',
+    //     cancelButtonColor: '#d33',
+    //     confirmButtonText: 'Yes, delete it!'
+    //   }).then(function () {
+    //     swal(
+    //       'Deleted!',
+    //       'Your file has been deleted.',
+    //       'success'
+    //     )
+    //   })
     
 }]);
